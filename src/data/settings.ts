e72444974fbd1e923db233f1852937509b31c7f1
Son Dinh <son@erptech.com.au>
2023-07-18 01:57:27 -0400

    Update

--- src/data/settings.ts
+++ src/data/settings.ts
@@ -1,6 +1,6 @@
 export const settings = {
-  site: 'https://cannonball.littlesticks.dev',
-  name: 'Little Sticks',
-  title: 'Cannonball by Little Sticks | A splash page template',
-  description: 'This is a simple splash page template built with Astro by Little Sticks',
+  site: 'https://i4sdigital.au',
+  name: 'I4S Digital',
+  title: 'I4S Digital | Digitalize Your Business',
+  description: 'I4S Digital is a digital agency that helps businesses to embark on their digital transformation journey.',
 }
--- src/data/socials.ts
+++ src/data/socials.ts
@@ -1,27 +1,12 @@
 export const socials = [
   {
-    title: 'Instagram',
-    icon: 'mdi:instagram',
-    url: 'https://www.instagram.com/jaydanurwin/',
-  },
-  {
-    title: 'Twitter',
-    icon: 'mdi:twitter',
-    url: 'https://twitter.com/littlesticksdev',
-  },
-  {
-    title: 'YouTube',
-    icon: 'mdi:youtube',
-    url: 'https://youtube.com/c/LittleSticks',
+    title: 'Crunchbase',
+    icon: 'cib:crunchbase',
+    url: 'https://www.crunchbase.com/organization/i4s-digital',
   },
   {
     title: 'GitHub',
     icon: 'mdi:github',
-    url: 'https://github.com/littlesticksdev',
-  },
-  {
-    title: 'Dribbble',
-    icon: 'mdi:dribbble',
-    url: 'https://dribbble.com/littlesticksdev',  
+    url: 'https://github.com/I4S-Digital',
   }
 ]
